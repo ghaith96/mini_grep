1ecc5135fb92b64793ff9f37b2ef594463d50fbe
ghaith96 <ghaithjardane@gmail.com>
2020-05-25 12:01:37 +0300

    redirect error to stderr

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing args: {}", err);
+        eprintln!("Problem parsing args: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Problem reading the file: {}", e);
+        eprintln!("Problem reading the file: {}", e);
         process::exit(1);
     }
 }
